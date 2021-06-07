7c389bd8dcf0289cd88e2aa6f080da1466c3ca54
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-07 09:55:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { token } = require("./root/configuration.json");
+
+const discord = require("discord.js");
+
+const client = new discord.Client({
+  disableEveryone: true 
+});
+
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+["command", "events"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+
+
+client.login(token);
