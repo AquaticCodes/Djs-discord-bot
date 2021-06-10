138bfac96841bbf7e4c1d9da011209e67a927651
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-10 12:52:58 +0530

    Create choose.js

---
+++ commands/fun/choose.js
@@ -0,0 +1,20 @@
+const random = require("../../functions/random.js");
+
+module.exports = {
+name: "choose"
+owner: false,
+admins: false,
+incognito: false,
+category: "fun",
+run: async (client, message, args) => {
+
+const tochoosefrom = args.join(" ");
+
+if (!tochoosefrom) {
+return message.lineReply("Please Give 2 Options To Choose From, Example: choose Say Hi || Say Hello");
+};
+
+message.channel.send("Hmm, I Choose \n" + random.choose(tochoosefrom));
+
+},
+};
