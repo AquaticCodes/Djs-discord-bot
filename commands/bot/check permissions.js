043de6403c5b8386ddbce299f05f975c45bf1a63
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-10 09:08:26 +0530

    Update check permissions.js

--- commands/bot/check permissions.js
+++ commands/bot/check permissions.js
@@ -7,16 +7,17 @@ admin: false,
 category: "bot",
 run: async (client, message, args) => {
 
-const category = args[0];
-
 const msg = await message.channel.send("Hold On Setting Up Database");
 
 await db.set(`${message.guild.id}.checkingpermissions`, [])
 await msg.delete();
-if (!category) {
+if (!args[0]) {
 return message.lineReplyNoMention("Oi, You Need To Specify The Commands Category \n checkperms <category> <optional inputs>");
 };
-const checking = await message.channel.send("Checking Permissions, Running The Required Functions and Scripted Code!!! \n Please Wait.. This Shouldn't Take Long..");
+
+const category = args[0].toLowerCase();
+
+const checking = await message.channel.send(`Checking Permissions For ${category}.., Running The Required Functions and Scripted Code!!! \n Please Wait.. This Shouldn't Take Long..`);
 if (category == "info") {
 
 if (!message.guild.me.hasPermission("EMBED_LINKS") {
@@ -31,8 +32,25 @@ const data = db.get(`${message.guild.id}.checkingpermissions`);
 
 await message.channel.send(data);
 await message.channel.send("ADMINISTRATOR permissions is sufficient for all categories and command!!");
+checking.delete();
+} else if (category == "mod" || category == "moderation") {
+
+if (!message.guild.me.hasPermission("MANAGE_GUILD") {
+await db.push(`${message.guild.id}.checkingpermissions`, "Fatal Permission Error: Serious, Guild \n NEEDED PERMISSIONS: MANAGE GUILD");
+}
+
+if (!message.guild.me.hasPermission("MANAGE_MESSAGES") {
+await db.push(`${message.guild.id}.checkingpermissions`, "Permissions: Ignorable, Messages \n NEEDED PERMISSIONS: MANAGE MESSAGES")
+}
+
+if (!message.guild.me.hasPermission("MANAGE_REACTIONS") {
+await db.push(`${message.guild.id}.checkingpermissions`, "Permissions: Ignorable, Messages/Reactions.manage \n NEEDED PERMISSIONS: MANAGE REACTIONS");
+}
+
+if (!message.guild.me.hasPermission(" ") {
 
 }
 
+}
 },
 };
