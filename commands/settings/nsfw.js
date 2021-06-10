63b013c996c106e6c4a48e9603b62f72b518a8a3
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-10 10:52:27 +0530

    Update nsfw.js

--- commands/settings/nsfw.js
+++ commands/settings/nsfw.js
@@ -12,11 +12,49 @@ run: async (client, message, args) => {
 
 // We Are Gonna Use Switch Statement 
 
-if (!args[0]) {
+if (!args[0]) { // using Auto-Toggle if user didn't specify to on/off
 
-}
+if (db.has(`${message.guild.id}.nsfw`)) {
+
+db.set(`${message.guild.id}.nsfw`, true)
+message.channel.send(
+"NSFW Settings Has Auto-Toggled, NSFW Commands Has Been Enabled!!!"
+);
+
+} else {
+
+db.delete(`${message.guild.id}.nsfw`, true)
+message.channel.send(
+"NSFW Settings Has Been Auto-Toggled, NSFW Commands Has Been Disabled!!!"
+);
+
+};
+
+};
 
 const toggle = args[0].toLowerCase()
+
+switch(toogle) {
+
+case "on":
+
+db.set(`${message.guild.id}.nsfw`, true)
+message.channel.send(
+"NSFW Settings Has Auto-Toggled, NSFW Commands Has Been Enabled!!!"
+);
+
+ break;
+
+case "off":
+
+db.delete(`${message.guild.id}.nsfw`, true)
+message.channel.send(
+"NSFW Settings Has Been Auto-Toggled, NSFW Commands Has Been Disabled!!!"
+);
+
+break;
+
+}
 
 },
 };
