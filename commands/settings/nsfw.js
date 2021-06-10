1bb0c9e091e78f87b67de201463f7c60d0da42a6
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-10 10:35:53 +0530

    Create nsfw.js

---
+++ commands/settings/nsfw.js
@@ -0,0 +1,22 @@
+const db = require("quick.db");
+
+module.exports = {
+name: "nsfw",
+owner: false,
+admins: true,
+incognito: true,
+category: "Settings",
+run: async (client, message, args) => {
+
+/* First Lets Create A Auto-Toggle Function, This basically is executed when there isn't any off/on specified :) */
+
+// We Are Gonna Use Switch Statement 
+
+if (!args[0]) {
+
+}
+
+const toggle = args[0].toLowerCase()
+
+},
+};
