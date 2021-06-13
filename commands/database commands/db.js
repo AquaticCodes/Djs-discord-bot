e44c55dfb1304676ad8c2341ac65a06883c4d075
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-13 21:03:20 +0530

    Update db.js

--- commands/database commands/db.js
+++ commands/database commands/db.js
@@ -24,10 +24,34 @@ if (todo == "add"||"subtract"||"set"||"push" && !function) {
 return message.lineReplyNoMention("Please Give A Value To Do Set | Add | subtract | push");
 }
 
-if (todo == "add"||"subtract"||"set"||"push") {
-db.todo(`${message.guild.id}${key}`, ${functions});
+if (todo == "add") {
+db.add(`${message.guild.id}${key}`, functions).catch(e => { message.channel.send(e); });
+message.channel.send(`${todo} for ${message.guild.id}${key} has been done successfully!!!`);
+} else if (todo == "subtract") {
+db.subtract(`${message.guild.id}${key}`, functions).catch(e => { message.channel.send(e); });
+message.channel.send(`${todo} for ${message.guild.id}${key} has been done successfully!!!`);
+} else if (todo == "set") {
+db.set(`${message.guild.id}${key}`, functions).catch(e => { message.channel.send(e); });
+message.channel.send(`${todo} for ${message.guild.id}${key} has been done successfully!!!`);
+} else if (todo == "push") {
+db.push(`${message.guild.id}${key}`, functions).catch(e => { message.channel.send(e); });
+message.channel.send(`${todo} for ${message.guild.id}${key} has been done successfully!!!`);
+} else if (todo == "delete") {
+if (function) {
+db.delete(`${message.guild.id}${key}`, functions).catch(e => { message.channel.send(e); });
 } else {
-db.todo(`${message.guild.id}${key}`);
+db.delete(`${message.guild.id}${key}`).catch(e => { message.channel.send(e); });
+}
+message.channel.send(`${todo} for ${message.guild.id}${key} has been done successfully!!!`);
+} else if (todo == "all") {
+message.channel.send(db.all(`${message.guild.id}${key}`)).catch(e => { message.channel.send(e); });
+} else if (todo == "get") {
+message.channel.send(db.get(`${message.guild.id}${key}`)).catch(e => { message.channel.send(e); });
+} else if (todo == "fetch") {
+message.channel.send(db.fetch(`${message.guild.id}${key}`).catch(e => { message.channel.send(e); });
+}
+} else {
+message.channel.send(`Database Functionality Error: At ${args.join(" ")}, ${args[0]} \n \n ${args[0]}(Function Error:65:31, no such function exists)`)
 }
 
 },
