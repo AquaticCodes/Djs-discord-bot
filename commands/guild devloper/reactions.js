b23015f40f73dd4b46b9131b7fecaa9bf851d4fa
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-14 14:31:04 +0530

    Update reactions.js

--- commands/guild devloper/reactions.js
+++ commands/guild devloper/reactions.js
@@ -9,7 +9,7 @@ category: "dev",
 authorPermission: ["ADMINISTRATOR"],
 run: async (client, message, args) => {
 
-consg msg = await message.channel.send("Hold On, Checking Activity And Version and changing it");
+const msg = await message.channel.send("Hold On, Checking Activity And Version and changing it");
 
 if (!db.has(`${message.guild.id}_reactionjs`)) {
 await db.set(`${message.guild.id}_reactionjs`);
