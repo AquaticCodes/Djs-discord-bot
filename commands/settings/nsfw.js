00e96fb33e7737d935e27854e2ffa30916de5431
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-14 14:16:47 +0530

    Update nsfw.js

--- commands/settings/nsfw.js
+++ commands/settings/nsfw.js
@@ -11,18 +11,18 @@ run: async (client, message, args) => {
 /* First Lets Create A Auto-Toggle Function, This basically is executed when there isn't any off/on specified :) */
 
 // We Are Gonna Use Switch Statement 
-
+var moded;
 if (!args[0]) { // using Auto-Toggle if user didn't specify to on/off
 
 if (!db.has(`${message.guild.id}.nsfw`)) {
-
+moded = "enabled"
 db.set(`${message.guild.id}.nsfw`, true)
 message.channel.send(
 "NSFW Settings Has Auto-Toggled, NSFW Commands Has Been Enabled!!!"
 );
 
 } else {
-
+moded = "disabled"
 db.delete(`${message.guild.id}.nsfw`, true)
 message.channel.send(
 "NSFW Settings Has Been Auto-Toggled, NSFW Commands Has Been Disabled!!!"
@@ -36,7 +36,7 @@ const toggle = args[0].toLowerCase()
 switch(toogle) {
 
 case "on":
-
+moded = "enabled"
 db.set(`${message.guild.id}.nsfw`, true)
 message.channel.send(
 "NSFW Settings Has Auto-Toggled, NSFW Commands Has Been Enabled!!!"
@@ -45,7 +45,7 @@ message.channel.send(
  break;
 
 case "off":
-
+moded = "disabled"
 db.delete(`${message.guild.id}.nsfw`, true)
 message.channel.send(
 "NSFW Settings Has Been Auto-Toggled, NSFW Commands Has Been Disabled!!!"
@@ -54,6 +54,12 @@ message.channel.send(
 break;
 }
 
+if (!db.has(`$message.guild.id}_cmd_nsfw`)) {
+db.set(`$message.guild.id}_cmd_nsfw`, []);
+db.push(`$message.guild.id}_cmd_nsfw`, `${message.author.tag} Has ${moded} NSFW`);
+} else {
+db.push(`$message.guild.id}.prefix`, `${message.author.tag} Has ${moded} NSFW`);
+}
 
 },
 };
