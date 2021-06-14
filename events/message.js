bd8abba2fd745a9a5d8fa2ced9b7d78e04cf2b89
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-14 07:50:59 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -69,6 +69,10 @@ return message.lineReplyNoMention("NSFW Settings Isn't Active, NSFW Commands Can
 if (!db.has(`${message.guild.id}_devmode`)) {
 return message.lineReplyNoMention("Developer Settings Has To Be Enabled To Use These Commands!!!");
 }
+} else if (command.root) {
+if (!db.has(`${message.guild.id}_root`) {
+return message.lineReply("Oi, No Root Detected, Is Root Installed?");
+}
 }
 
 /* P E R M I S S I O N S */
