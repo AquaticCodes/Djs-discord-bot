ab51aa96fb74dd38b1117b98d6b21170d730ba36
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-22 10:50:52 +0530

    Create memes.js

---
+++ commands/fun/memes.js
@@ -0,0 +1,14 @@
+const db = require("quick.db");
+const { MessageEmbed } = require("discord.js");
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+
+module.exports = {
+name: "memes",
+aliases: ["meme"],
+owner: false,
+admins: false,
+category: "fun",
+run: async (client, message, args) => {
+
+},
+};
