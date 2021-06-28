bd2df7c6c927f73dcd93f3b96adce7591cd02d23
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-28 11:49:29 +0530

    Create auto mod.js

---
+++ commands/settings/auto mod.js
@@ -0,0 +1,13 @@
+const db = require("quick.db");
+
+module.exports = {
+name: "automod",
+owner: false,
+admins: false,
+authorPermission: ["MANAGE_GUILD"],
+run: async (client, message, args) => {
+
+
+
+},
+};
