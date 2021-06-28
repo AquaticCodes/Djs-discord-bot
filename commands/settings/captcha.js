ba5211c8ca4848a865ed31f0b04fde434ca29a75
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-28 11:40:44 +0530

    Create captcha.js

---
+++ commands/settings/captcha.js
@@ -0,0 +1,22 @@
+const db = require('quick.db')
+
+module.exports = {
+    name : 'captcha',
+    owner: false,
+    admins: false,
+    category: "settings",
+    run : async(client, message, args) => {
+
+if (!args[0]) {
+return message.lineReply("Specify On/Off");
+}
+
+        if(args[0].toLowerCase() === 'on') {
+            await db.set(`captcha-${message.guild.id}`, true)
+            message.channel.send('Turned on captcha feature')
+        } else if(args[0].toLowerCase() === 'off') {
+            await db.delete(`captcha-${message.guild.id}`)
+            message.channel.send('Turned off captcha feature')
+        }
+    }
+}
