71b9c4ec0a8afb55d7e1d4a6eea6d82b8d1448a5
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-06-28 11:38:17 +0530

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,14 +5,61 @@ const discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, member) => {
-  const message = db.get(`${member.guild.id}_welcomer`);
+  const welcome = db.get(`${member.guild.id}_welcomer`);
   
+const first = welcome.replace("${user.username}", `${member.user.username}`);
+
+const second = first.replace("${user.tag}", `<@${member.user.id}>`);
+
+const third = second.replace("${user.syntaxTag}", `${member.user.tag}`);
+
+const message = third.replace("${server}", `${member.guild.name}`);
+
+
   let embed = new MessageEmbed()
 .setTitle(`Welcome ${member.user.tag}`)
 .setColor("RANDOM")
 .setDescription(message)
 .setFooter(`You're ${member.guild.memberCount + 1} Member Of The Guild`)
 .setTimestamp();
+
+if(db.has(`captcha-${member.guild.id}`)=== false) return;
+    const url = 'https://api.no-api-key.com/api/v2/captcha';
+        try {
+            fetch(url)
+                .then(res => res.json())
+                .then(async json => {
+                    console.log(json)
+                    const msg = await member.send(
+                        new MessageEmbed()
+                            .setTitle('Please enter the captcha')
+                            .setImage(json.captcha)
+                            .setColor("RANDOM")
+                    )
+                    try {
+                        const filter = (m) => {
+                            if(m.author.bot) return;
+                            if(m.author.id === member.id && m.content === json.captcha_text) return true;
+                            else {
+                                msg.channel.send("You have answered the captcha incorrectly!")
+                            }
+                        };
+                        const response = await msg.channel.awaitMessages(filter, {
+                            max : 1,
+                            time : 10000,
+                            errors : ['time']
+                        })
+                        if(response) {
+                            msg.channel.send('Congrats, you have answered the captcha.')
+                        }
+                    } catch (error) {
+                        msg.channel.send(`You have been kicked from **${member.guild.name}** for not answering the captcha correctly.`)
+                        member.kick()
+                    }
+                })
+        } catch (error) {
+            console.log(error)
+        }
 
 try {
 
